d9282c77765b8bf719b484d69ec3d14427a8662a
debug <debug@5063f80b-35bb-49d0-95f4-ce7ec8300d17>
2014-08-03 12:01:21 +0000

    Make debug messages less verbose when running experiments with my physical Dreamcast's ROM image.
    git-svn-id: https://svn.code.sf.net/p/gxemul/code2/gxemul/trunk@5818 5063f80b-35bb-49d0-95f4-ce7ec8300d17

--- src/devices/dev_dreamcast_maple.cc
+++ src/devices/dev_dreamcast_maple.cc
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2006-2011  Anders Gavare.  All rights reserved.
+ *  Copyright (C) 2006-2014  Anders Gavare.  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions are met:
@@ -371,7 +371,7 @@ void maple_do_dma_xfer(struct cpu *cpu, struct dreamcast_maple_data *d)
 	uint32_t addr = d->dmaaddr;
 
 	if (!d->enable) {
-		fatal("[ maple_do_dma_xfer: not enabled? ]\n");
+		debug("[ maple_do_dma_xfer: not enabled? ]\n");
 		return;
 	}
 
