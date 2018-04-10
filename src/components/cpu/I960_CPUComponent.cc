497d5cccfe5e81a933900bddea99ef0f89754ebe
debug <debug@5063f80b-35bb-49d0-95f4-ce7ec8300d17>
2018-04-10 08:20:44 +0000

    Adding a dummy skeleton for an Intel i960 CPU component using the new framework.
    git-svn-id: https://svn.code.sf.net/p/gxemul/code2/gxemul/trunk@5886 5063f80b-35bb-49d0-95f4-ce7ec8300d17

---
+++ src/components/cpu/I960_CPUComponent.cc
@@ -0,0 +1,371 @@
+/*
+ *  Copyright (C) 2018  Anders Gavare.  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright  
+ *     notice, this list of conditions and the following disclaimer in the 
+ *     documentation and/or other materials provided with the distribution.
+ *  3. The name of the author may not be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE   
+ *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ *  OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ *  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ *  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ *  SUCH DAMAGE.
+ */
+
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <iomanip>
+
+#include "ComponentFactory.h"
+#include "GXemul.h"
+#include "components/I960_CPUComponent.h"
+
+
+I960_CPUComponent::I960_CPUComponent()
+	: CPUDyntransComponent("i960_cpu", "i960")
+{
+	m_frequency = 25e6;
+
+	ResetState();
+
+//	AddVariable("hi", &m_hi);
+//	AddVariable("lo", &m_lo);
+
+	// TODO: This only registers using the new ABI names. How should
+	// this be handled? Custom "aliasing" variables?
+//	for (size_t i=0; i<N_I960_REGS; i++)
+//		AddVariable(regname(i, m_abi), &m_gpr[i]);
+}
+
+
+refcount_ptr<Component> I960_CPUComponent::Create(const ComponentCreateArgs& args)
+{
+	// Defaults:
+	ComponentCreationSettings settings;
+	settings["model"] = "i960CA";
+
+	if (!ComponentFactory::GetCreationArgOverrides(settings, args))
+		return NULL;
+
+	refcount_ptr<Component> cpu = new I960_CPUComponent();
+//	if (!cpu->SetVariableValue("model", "\"" + settings["model"] + "\""))
+//		return NULL;
+
+	return cpu;
+}
+
+
+void I960_CPUComponent::ResetState()
+{
+	m_pageSize = 4096;
+
+	for (size_t i=0; i<N_I960_REGS; i++)
+		m_r[i] = 0;
+
+	m_pc = 0;
+
+	CPUDyntransComponent::ResetState();
+}
+
+
+bool I960_CPUComponent::PreRunCheckForComponent(GXemul* gxemul)
+{
+	if (m_pc & 0x3) {
+		gxemul->GetUI()->ShowDebugMessage(this, "the pc register"
+		    " can not have bit 0 or 1 set!\n");
+		return false;
+	}
+
+	return CPUDyntransComponent::PreRunCheckForComponent(gxemul);
+}
+
+
+bool I960_CPUComponent::CheckVariableWrite(StateVariable& var, const string& oldValue)
+{
+	// UI* ui = GetUI();
+
+	return CPUDyntransComponent::CheckVariableWrite(var, oldValue);
+}
+
+
+void I960_CPUComponent::ShowRegisters(GXemul* gxemul, const vector<string>& arguments) const
+{
+	stringstream ss;
+
+	ss.flags(std::ios::hex);
+	ss << std::setfill('0');
+
+	// Yuck, this is horrible. Is there some portable way to put e.g.
+	// std::setw(16) into an object, and just pass that same object several
+	// times?
+
+	ss << "pc=";
+	ss << std::setw(8);
+	ss << (uint32_t)m_pc;
+
+	string symbol = GetSymbolRegistry().LookupAddress(m_pc, true);
+	if (symbol != "")
+		ss << " <" << symbol << ">";
+	ss << "\n";
+/*
+	ss << "hi=";
+	if (is32bit)
+		ss << std::setw(8);
+	else
+		ss << std::setw(16);
+	ss << Trunc3264(m_hi, is32bit) << " lo=";
+	if (is32bit)
+		ss << std::setw(8);
+	else
+		ss << std::setw(16);
+	ss << Trunc3264(m_lo, is32bit) << "\n";
+
+	for (size_t i=0; i<N_I960_REGS; i++) {
+		ss << regname(i, m_abi) << "=";
+		if (is32bit)
+			ss << std::setw(8);
+		else
+			ss << std::setw(16);
+		ss << Trunc3264(m_gpr[i], is32bit);
+		if ((i&3) == 3)
+			ss << "\n";
+		else
+			ss << " ";
+	}
+*/
+	gxemul->GetUI()->ShowDebugMessage(ss.str());
+}
+
+
+int I960_CPUComponent::FunctionTraceArgumentCount()
+{
+	// TODO
+	return 8;
+}
+
+
+int64_t I960_CPUComponent::FunctionTraceArgument(int n)
+{
+	// TODO
+	return m_r[n];
+}
+
+
+bool I960_CPUComponent::FunctionTraceReturnImpl(int64_t& retval)
+{
+	// TODO
+	retval = m_r[0];
+	return true;
+}
+
+
+int I960_CPUComponent::GetDyntransICshift() const
+{
+	// 4 bytes per instruction means 2 bits shift.
+	return 2;
+}
+
+
+void (*I960_CPUComponent::GetDyntransToBeTranslated())(CPUDyntransComponent*, DyntransIC*)
+{
+	return instr_ToBeTranslated;
+}
+
+
+bool I960_CPUComponent::VirtualToPhysical(uint64_t vaddr, uint64_t& paddr,
+	bool& writable)
+{
+	paddr = vaddr;
+	writable = true;
+	return true;
+}
+
+
+uint64_t I960_CPUComponent::PCtoInstructionAddress(uint64_t pc)
+{
+	return pc;
+}
+
+
+size_t I960_CPUComponent::DisassembleInstruction(uint64_t vaddr, size_t maxLen,
+	unsigned char *instruction, vector<string>& result)
+{
+	const size_t instrSize = sizeof(uint32_t);
+
+	if (maxLen < instrSize) {
+		assert(false);
+		return 0;
+	}
+
+	// Read the instruction word:
+	uint32_t instructionWord = *((uint32_t *) (void *) instruction);
+	if (m_isBigEndian)
+		instructionWord = BE32_TO_HOST(instructionWord);
+	else
+		instructionWord = LE32_TO_HOST(instructionWord);
+
+	const uint32_t iword = instructionWord;
+
+	// ... and add it to the result:
+	{
+		stringstream ss;
+		ss.flags(std::ios::hex);
+		ss << std::setfill('0') << std::setw(8) << (uint32_t) iword;
+		if (PCtoInstructionAddress(m_pc) == vaddr && m_inDelaySlot)
+			ss << " (delayslot)";
+		result.push_back(ss.str());
+	}
+
+	const int opcode = iword >> 24;
+
+	switch (opcode) {
+
+	default:
+		{
+			stringstream ss;
+			ss << "unimplemented: " << opcode;
+			result.push_back(ss.str());
+		}
+		break;
+	}
+
+	return instrSize;
+}
+
+
+string I960_CPUComponent::GetAttribute(const string& attributeName)
+{
+	if (attributeName == "stable")
+		return "yes";
+
+	if (attributeName == "description")
+		return "Intel i960 processor.";
+
+	return Component::GetAttribute(attributeName);
+}
+
+
+/*****************************************************************************/
+
+
+
+/*
+DYNTRANS_INSTR(I960_CPUComponent,multu)
+{
+	DYNTRANS_INSTR_HEAD(I960_CPUComponent)
+
+	uint32_t a = REG64(ic->arg[1]), b = REG64(ic->arg[2]);
+	uint64_t res = (uint64_t)a * (uint64_t)b;
+
+	cpu->m_lo = (int32_t)res;
+	cpu->m_hi = (int32_t)(res >> 32);
+}
+
+
+DYNTRANS_INSTR(I960_CPUComponent,slt)
+{
+	REG64(ic->arg[0]) = (int64_t)REG64(ic->arg[1]) < (int64_t)REG64(ic->arg[2]);
+}
+*/
+
+
+/*****************************************************************************/
+
+
+void I960_CPUComponent::Translate(uint32_t iword, struct DyntransIC* ic)
+{
+	bool singleInstructionLeft = (m_executedCycles == m_nrOfCyclesToExecute - 1);
+	UI* ui = GetUI();	// for debug messages
+
+	unsigned int opcode = iword >> 24;
+
+	switch (opcode) {
+
+	default:
+		if (ui != NULL) {
+			stringstream ss;
+			ss.flags(std::ios::hex);
+			ss << "unimplemented opcode 0x" << opcode;
+			ui->ShowDebugMessage(this, ss.str());
+		}
+	}
+}
+
+
+DYNTRANS_INSTR(I960_CPUComponent,ToBeTranslated)
+{
+	DYNTRANS_INSTR_HEAD(I960_CPUComponent)
+
+	cpu->DyntransToBeTranslatedBegin(ic);
+
+	uint32_t iword;
+	if (cpu->DyntransReadInstruction(iword))
+		cpu->Translate(iword, ic);
+
+	cpu->DyntransToBeTranslatedDone(ic);
+}
+
+
+/*****************************************************************************/
+
+
+#ifdef WITHUNITTESTS
+
+#include "ComponentFactory.h"
+
+static void Test_I960_CPUComponent_Create()
+{
+	refcount_ptr<Component> cpu = ComponentFactory::CreateComponent("i960_cpu");
+	UnitTest::Assert("component was not created?", !cpu.IsNULL());
+
+	const StateVariable * p = cpu->GetVariable("pc");
+	UnitTest::Assert("cpu has no pc state variable?", p != NULL);
+	UnitTest::Assert("initial pc", p->ToString(), "0");
+}
+
+static void Test_I960_CPUComponent_Disassembly_Basic()
+{
+	refcount_ptr<Component> i960_cpu =
+	    ComponentFactory::CreateComponent("i960_cpu");
+	CPUComponent* cpu = i960_cpu->AsCPUComponent();
+
+	vector<string> result;
+	size_t len;
+	unsigned char instruction[sizeof(uint32_t)];
+	// This assumes that the default endianness is BigEndian...
+	instruction[0] = 0x27;
+	instruction[1] = 0xbd;
+	instruction[2] = 0xff;
+	instruction[3] = 0xd8;
+
+	len = cpu->DisassembleInstruction(0x12345678, sizeof(uint32_t),
+	    instruction, result);
+
+	UnitTest::Assert("disassembled instruction was wrong length?", len, 4);
+	UnitTest::Assert("disassembly result incomplete?", result.size(), 3);
+	UnitTest::Assert("disassembly result[0]", result[0], "27bdffd8");
+	UnitTest::Assert("disassembly result[1]", result[1], "addiu");
+	UnitTest::Assert("disassembly result[2]", result[2], "sp,sp,-40");
+}
+
+UNITTESTS(I960_CPUComponent)
+{
+	UNITTEST(Test_I960_CPUComponent_Create);
+	UNITTEST(Test_I960_CPUComponent_Disassembly_Basic);
+}
+
+#endif
---
+++ src/include/components/I960_CPUComponent.h
@@ -0,0 +1,139 @@
+#ifndef I960_CPUCOMPONENT_H
+#define	I960_CPUCOMPONENT_H
+
+/*
+ *  Copyright (C) 2018  Anders Gavare.  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright  
+ *     notice, this list of conditions and the following disclaimer in the 
+ *     documentation and/or other materials provided with the distribution.
+ *  3. The name of the author may not be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE   
+ *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ *  OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ *  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ *  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ *  SUCH DAMAGE.
+ */
+
+// COMPONENT(i960_cpu)
+
+
+#include "CPUDyntransComponent.h"
+
+
+#define	N_I960_REGS		32
+
+
+/*
+
+https://people.cs.clemson.edu/~mark/subroutines/i960.html
+
+Register conventions
+
+        g0-g3    - parameters 0-3  /  return words 0-3
+        g4-g7    - parameters 4-7  /  temporaries
+        g8-g12   - preserved across call if not used for parameters
+        g13      - structure return pointer
+        g14      - argument block pointer  /  leaf return address (hardware)
+        fp (g15) - frame pointer (hardware, aligned on 16-byte boundaries)
+
+        pfp (r0) - previous frame pointer (hardware)
+        sp  (r1) - stack pointer (hardware)
+        rip (r2) - return instruction pointer (hardware)
+        r3-r15   - available as locals for subroutine
+
+*/
+
+
+/***********************************************************************/
+
+
+/**
+ * \brief A Component representing an Intel i960 processor.
+ */
+class I960_CPUComponent
+	: public CPUDyntransComponent
+{
+public:
+	/**
+	 * \brief Constructs a I960_CPUComponent.
+	 */
+	I960_CPUComponent();
+
+	/**
+	 * \brief Creates a I960_CPUComponent.
+	 */
+	static refcount_ptr<Component> Create(const ComponentCreateArgs& args);
+
+	static string GetAttribute(const string& attributeName);
+
+	virtual void ResetState();
+
+	virtual bool PreRunCheckForComponent(GXemul* gxemul);
+
+	virtual size_t DisassembleInstruction(uint64_t vaddr, size_t maxlen,
+		unsigned char *instruction, vector<string>& result);
+
+
+	/********************************************************************/
+
+	static void RunUnitTests(int& nSucceeded, int& nFailures);
+
+protected:
+	virtual bool CheckVariableWrite(StateVariable& var, const string& oldValue);
+
+	virtual bool VirtualToPhysical(uint64_t vaddr, uint64_t& paddr, bool& writable);
+
+	virtual string VirtualAddressAsString(uint64_t vaddr)
+	{
+		stringstream ss;
+		ss.flags(std::ios::hex | std::ios::showbase | std::ios::right);
+		ss << (uint32_t)vaddr;
+		return ss.str();
+	}
+
+	virtual uint64_t PCtoInstructionAddress(uint64_t pc);
+
+	virtual int FunctionTraceArgumentCount();
+	virtual int64_t FunctionTraceArgument(int n);
+	virtual bool FunctionTraceReturnImpl(int64_t& retval);
+
+	virtual int GetDyntransICshift() const;
+	virtual void (*GetDyntransToBeTranslated())(CPUDyntransComponent*, DyntransIC*);
+
+	virtual void ShowRegisters(GXemul* gxemul, const vector<string>& arguments) const;
+
+private:
+	template<int op, bool samepage, bool singlestep> static void instr_b(CPUDyntransComponent* cpubase, DyntransIC* ic);
+	template<bool link, bool singlestep> static void instr_j(CPUDyntransComponent* cpubase, DyntransIC* ic);
+	template<bool link, bool singlestep> static void instr_jr(CPUDyntransComponent* cpubase, DyntransIC* ic);
+
+	DECLARE_DYNTRANS_INSTR(multu);
+
+	template<bool store, typename addressType, typename T, bool signedLoad> static void instr_loadstore(CPUDyntransComponent* cpubase, DyntransIC* ic);
+
+	void Translate(uint32_t iword, struct DyntransIC* ic);
+	DECLARE_DYNTRANS_INSTR(ToBeTranslated);
+
+private:
+	/*
+	 * State:
+	 */
+	uint32_t	m_r[N_I960_REGS];
+};
+
+
+#endif	// I960_CPUCOMPONENT_H
