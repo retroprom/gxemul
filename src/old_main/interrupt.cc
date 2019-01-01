451a5c2cc6dd6e4a0dcaecebd7e19a4c91556555
debug <debug@5063f80b-35bb-49d0-95f4-ce7ec8300d17>
2019-01-01 19:17:07 +0000

    Making it less frustrating to develop new code connecting to irqs: when lookup fails, list the path names that are actually available.
    git-svn-id: https://svn.code.sf.net/p/gxemul/code2/gxemul/trunk@6132 5063f80b-35bb-49d0-95f4-ce7ec8300d17

--- src/old_main/interrupt.cc
+++ src/old_main/interrupt.cc
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2006-2009  Anders Gavare.  All rights reserved.
+ *  Copyright (C) 2006-2019  Anders Gavare.  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions are met:
@@ -188,8 +188,13 @@ int interrupt_handler_lookup(const char *name, struct interrupt *templ)
 
 	/*  The 'if' is an ugly hack to prevent a Compaq CC warning.  */
 	if (i >= nr_of_interrupt_handlers) {
-		printf("interrupt_handler_lookup(\"%s\") failed. "
-		    "Aborting.\n", name);
+		printf("interrupt_handler_lookup(\"%s\") failed.\n", name);
+
+		printf("Available handler paths are:\n");
+		for (i=0; i<nr_of_interrupt_handlers; i++)
+			printf("    %s\n", interrupt_handlers[i].templ.name);
+
+		printf("Aborting.\n");
 		abort();
 	}
 
