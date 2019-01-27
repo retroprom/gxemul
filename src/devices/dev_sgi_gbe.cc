19fea97ab8a6b2265c36aabd5063756127fe2746
debug <debug@5063f80b-35bb-49d0-95f4-ce7ec8300d17>
2019-01-27 13:24:51 +0000

    Fixing potential compiler warning/error.
    git-svn-id: https://svn.code.sf.net/p/gxemul/code2/gxemul/trunk@6145 5063f80b-35bb-49d0-95f4-ce7ec8300d17

--- src/devices/dev_sgi_gbe.cc
+++ src/devices/dev_sgi_gbe.cc
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2003-2018  Anders Gavare.  All rights reserved.
+ *  Copyright (C) 2003-2019  Anders Gavare.  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions are met:
@@ -58,7 +58,7 @@
 
 
 /*  Let's hope nothing is there already...  */
-#define	FAKE_GBE_FB_ADDRESS	0x380000000
+#define	FAKE_GBE_FB_ADDRESS	0x380000000ULL
 
 
 // #define	GBE_DEBUG
