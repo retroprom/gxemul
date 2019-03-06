df6c45ad1a1a2e8f5309d25ff7c0a6d487b37391
debug <debug@5063f80b-35bb-49d0-95f4-ce7ec8300d17>
2019-03-06 04:52:51 +0000

    Applying a patch from Andreas Gustafsson (gson) to set the SA_RESTART flag in the timer sigaction call.
    git-svn-id: https://svn.code.sf.net/p/gxemul/code2/gxemul/trunk@6159 5063f80b-35bb-49d0-95f4-ce7ec8300d17

--- src/old_main/timer.cc
+++ src/old_main/timer.cc
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2006-2018  Anders Gavare.  All rights reserved.
+ *  Copyright (C) 2006-2019  Anders Gavare.  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions are met:
@@ -229,6 +229,7 @@ void timer_start(void)
 
 	memset(&saction, 0, sizeof(saction));
 	saction.sa_handler = timer_tick;
+	saction.sa_flags = SA_RESTART;
 
 	sigaction(SIGALRM, &saction, NULL);
 
