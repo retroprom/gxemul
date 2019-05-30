102c4f8326658296885fde810bded3e2e1f38c5d
debug <debug@5063f80b-35bb-49d0-95f4-ce7ec8300d17>
2019-05-30 18:15:06 +0000

    Removing the QEMU_MIPS machine mode, which was useful back in 2006 to compare performance against QEMU's MIPS emulation.
    git-svn-id: https://svn.code.sf.net/p/gxemul/code2/gxemul/trunk@6208 5063f80b-35bb-49d0-95f4-ce7ec8300d17

--- src/devices/dev_pccmos.cc
+++ src/devices/dev_pccmos.cc
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2005-2011  Anders Gavare.  All rights reserved.
+ *  Copyright (C) 2005-2019  Anders Gavare.  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions are met:
@@ -135,7 +135,6 @@ DEVINIT(pccmos)
 		/*  Malta etc.  */
 		type = MC146818_ALGOR;
 		break;
-	case MACHINE_QEMU_MIPS:
 	case MACHINE_COBALT:
 	case MACHINE_PREP:
 	case MACHINE_MVMEPPC:
