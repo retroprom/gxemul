8fe9628750bb732822281cf7ec343fbb517f5b86
debug <debug@5063f80b-35bb-49d0-95f4-ce7ec8300d17>
2021-01-06 07:38:27 +0000

    Adding stdio.h include, required for build with some gcc versions on Linux. Noticed by Michael Mikonos.
    git-svn-id: https://svn.code.sf.net/p/gxemul/code2/gxemul/trunk@6300 5063f80b-35bb-49d0-95f4-ce7ec8300d17

--- src/ui/console/ConsoleUI.cc
+++ src/ui/console/ConsoleUI.cc
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2007-2019  Anders Gavare.  All rights reserved.
+ *  Copyright (C) 2007-2021  Anders Gavare.  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions are met:
@@ -26,6 +26,7 @@
  */
 
 #include <signal.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <cstdio>
 #include <iostream>
